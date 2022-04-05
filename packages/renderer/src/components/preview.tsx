66a48cc85ad612eabc3a308ff89bccb26265b61c
James Troughton <jamesstroughton@gmail.com>
2022-04-05 23:35:55 +1200

    🎨

---
+++ packages/renderer/src/components/preview.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Preview = () => {
+  return <div className="preview-wrapper">{/* TODO */}</div>
+}
+
+export default Preview
--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import './index.css'
+import './styles/index.css'
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
